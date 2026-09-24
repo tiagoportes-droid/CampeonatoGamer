a1b17e43373183b184c704d1291cd058741513a5
TIAGO FERREIRA DA LUZ PORTES <26171637@edu119.local>
2026-09-24 09:38:45 -0300

    feat: resolve multiple bugs in the code

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-import express from express()
-import cors from cors()
+import express from "express";
+import cors from "cors";
 
-const app = express()
-app.use(cors()) // Middleware = usa o front end
-app.use(express.json) // faz ele entender o json
+const app = express();
+app.use(cors()); // Middleware = usa o front end
+app.use(express.json()); // faz ele entender o json
 
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Campeonato Gamer no Ar!" });
+});
 
-app.get("/", (req, res) =>{
-
-})
-
-const PORTA = 3000
-app.listen(PORTA, () => {})
+const PORTA = 3000;
+app.listen(PORTA, () => {
+  console.log(`Servidor rodando em http://localhost:${PORTA}`);
+});
